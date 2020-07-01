c42cd7098f237d093f40f1ba708a6b82b31405f6
arunteja-dev <aruntejagurrapu@gmail.com>
2020-07-01 20:39:48 +0530

    study material implementation

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour
+{
+    public Button button;
+    public string Scene2;
+
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnButtonClick()
+    {
+        Debug.Log("Button Clicked");
+        SceneManager.LoadScene(Scene2);
+    }
+}
---
+++ Assets/Scripts/Practice.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice :  MonoBehaviour  
+{
+    //Awake function is called if the object is created
+    private void Awake()
+    {
+        Debug.Log("Awake function to execute");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("first function to execute");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Update Input F button is pressed:");
+
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("Update Input F:");
+
+        }
+        if (Input.GetKeyUp(KeyCode.F) || Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Update Input F button is Released:");
+
+        }
+    }
+
+    //anything related to physics
+    private void LateUpdate()
+    {
+        
+    }
+}
